ef9609e9821a6e110ccc6fa797ab41c34dd86efd
Asafegustavo <gusta10342@gmail.com>
2026-09-22 22:29:10 -0300

    prova7

---
+++ main.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+char nome;
+float nota[4];
+int media;
+int i;
+int opcao; 
+int MediaTotal;
+float NotaFinal;
+int opcaoinvalida;
+
+void main() {
+
+
+if(
+
+("\n media = nota1 + nota2 + nota3 + nota4 ")
+("\n NotaFinal >= 7 ")
+("\n NotaFinal < 7 ")
+
+) {
+
+printf("\n1 - ");
+
+}
+
+switch() {
+
+case 1: 
+
+printf("\n nome do aluno: ");
+scanf("%d", nome);
+break;
+
+case 2:
+
+printf("\n nota1: ");
+printf("\n nota2: ");
+printf("\n nota3: ");
+printf("\n nota4: ");
+scanf("%.2f", nota);
+break;
+
+case 3:
+
+scanf("%f", MediaTotal);
+break;
+
+case 0:
+default:
+
+printf("opcao invalida! ");
+break;
+}
+
+
+
+
+
+
+
+
+
+
+}
